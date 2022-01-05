ffc512ed636875d51e1e2e519699acc437a8c35e
laszlopota <laszlopota00@gmail.com>
2022-01-05 16:40:18 +0100

    update: 22/01/05/1640

--- progressbar.py
+++ progressbar.py
@@ -11,13 +11,13 @@ class ProgressBar:
               'white': '\033[0m',
               'black': '\033[90m'}
 
-    def __init__(self, listLength, kvPairs):
+    def __init__(self, listLength, options):
         self.currentIndex = 0
         self.listLength = listLength
-        self.kvPairs = kvPairs
+        self.options = options
 
     def __next__(self):
-        barLength = self.kvPairs['length']
+        barLength = self.options['length']
         ratio = (len(str(self.listLength)) - len(str(self.currentIndex + 1))) * ' ' + f'{self.currentIndex + 1}/{self.listLength}'
         percentage = f'{int(100 * (self.currentIndex + 1) / self.listLength)}% '
 
@@ -29,11 +29,11 @@ class ProgressBar:
         barEnd = '│ '
 
         print('\r', end=' ')
-        print(self.colors[self.kvPairs['color']] + self.kvPairs['text'] + spacesInFront + percentage + barStart + barEmpty + barEnd + ratio, end='')
+        print(self.colors[self.options['color']] + self.options['text'] + spacesInFront + percentage + barStart + barEmpty + barEnd + ratio, end='')
         if self.currentIndex == self.listLength - 1:
             print('\033[0m', end='\n')
 
-        sleep(self.kvPairs['sleep'])
+        sleep(self.options['sleep'])
 
         self.currentIndex += 1
 
@@ -42,20 +42,20 @@ class ProgressBar:
     __call__ = __next__
 
 def progressbar(listLength, **kwargs):
-    kvPairs = {'color': 'white',
+    options = {'color': 'white',
                'text': '',
                'length': 30,
                'sleep': 0}
     for key, value in kwargs.items():
-        kvPairs[key] = value
+        options[key] = value
 
-    myProgressbar = iter(ProgressBar(listLength, kvPairs), listLength)
+    myProgressbar = iter(ProgressBar(listLength, options), listLength)
 
     return myProgressbar
 
 
 if __name__ == '__main__':
     print('This is my progressbar:')
-    for i in progressbar(5, color='red', text='Progression: ', length=40, sleep=1):
+    for i in progressbar(100, color='red', text='Progression: ', length=40, sleep=1):
         pass
     print('Done!')
